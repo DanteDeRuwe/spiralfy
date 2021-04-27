0995911f22a46c0963179edb6d1b34c66dfea959
Dante De Ruwe <dantederuwe@gmail.com>
2021-04-27 16:34:02 +0200

    Set up DI

--- Spiralfy/Program.cs
+++ Spiralfy/Program.cs
@@ -1,24 +1,34 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpotifyAPI.Web;
 
 namespace Spiralfy
 {
     public class Program
     {
+        private static IConfigurationRoot _configuration;
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            });
+            _configuration = builder.Configuration;
+            ConfigureServices(builder.Services);
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            Configure(host);
+
+            await host.RunAsync();
         }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(_ => new SpotifyClient(_configuration["SpotifyToken"]));
+        }
+
+        private static void Configure(WebAssemblyHost host) { }
     }
 }
