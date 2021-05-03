89741718533dd291cb93708c2beb85f8a6bb67bd
Dante De Ruwe <dantederuwe@gmail.com>
2021-05-03 14:15:51 +0200

    Create a player pilet that uses spotify web sdk
    it will be invisible, but will autoplay, and therefore make sure we don't rely on an external player!

---
+++ player/src/Player.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+import SpotifyPlayer from 'react-spotify-web-playback';
+const queryString = require('query-string');
+
+const Player: React.FC = () => {
+  const { hash } = useLocation();
+  const { access_token } = queryString.parse(hash);
+
+  return (
+    <div style={{ display: 'none' }}>
+      <SpotifyPlayer
+        token={access_token}
+        uris={['spotify:track:2bNCdW4rLnCTzgqUXTTDO1']} //this track is 4'33 by John Cage: 4 minutes and 33 seconds of silence
+        autoPlay={true}
+      />
+    </div>
+  );
+};
+
+export default Player;
--- player/src/index.tsx
+++ player/src/index.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
 import { PiletApi } from 'spiralfy-appshell';
+import Player from './Player';
 
-export function setup(app: PiletApi) {}
+export function setup(app: PiletApi) {
+  app.registerExtension('header-items', Player);
+}
