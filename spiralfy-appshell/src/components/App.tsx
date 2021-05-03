8700a3fea9e3207df187c0804a317e7b27b6a126
Dante De Ruwe <dantederuwe@gmail.com>
2021-05-03 11:50:46 +0200

    Clean up structure and styling

--- spiralfy-appshell/src/components/App.tsx
+++ spiralfy-appshell/src/components/App.tsx
@@ -1,24 +1,17 @@
 import { ExtensionSlot, LayoutProps } from 'piral-core';
 import { Menu } from 'piral-menu';
 import * as React from 'react';
-import Logo from './Logo';
 import Footer from './Footer';
-import { Route } from 'react-router-dom';
-import PopoutButton from './PopoutButton';
+import Logo from './Logo';
 
 const App: React.FC<LayoutProps> = ({ children }) => (
   <>
-    <div className="main-wrapper">
-      <header className="Header">
-        <Logo />
-        <Menu type="general" />
-        <ExtensionSlot name="header-items" />
-      </header>
-      <Route exact path="/">
-        <ExtensionSlot name="homepage" />
-      </Route>
-      {children}
-    </div>
+    <header className="Header">
+      <Logo />
+      <Menu type="general" />
+      <ExtensionSlot name="header-items" />
+    </header>
+    <main className="Main">{children}</main>
     <Footer />
   </>
 );
