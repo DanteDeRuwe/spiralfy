982f7894a1d9d32a50724199c1d90e45a8910599
Dante De Ruwe <dantederuwe@gmail.com>
2021-05-07 18:13:46 +0200

    Add an about page

---
+++ spiralfy-appshell/src/components/About.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+const About: React.FC = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col mb-5 d-flex justify-content-center">
+        <img src="https://i.imgur.com/DpDlQP9.jpg" height="500" />
+      </div>
+      <div className="col">
+        <h1 className="h1">Welcome to Spiralfy 🎉</h1>
+        <p>Spiralfy is a way to switch up your Spotify experience!</p>
+        <p>
+          Spiralfy picks one playlist at random, shuffle playing its songs, and whenever you feel like you want a
+          different vibe, you let Spiralfy pick a new playlist to listen to.
+        </p>
+        <p>
+          Unfortunately, you will need a <b>Spotify Premium account</b> to use Spiralfy. If you have one, great! Log in
+          using the button in the header!
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
